e2ee43d6b733550d76c92bd28bb6b736c2712a79
Spencer Owen <owenspencer@gmail.com>
2022-08-27 23:44:05 -0600

    parameterize led pin

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,21 @@
 #include <Arduino.h>
 #include <DigiMouse.h>
 
-// #define USB_CFG_VENDOR_ID 0xAC, 0x05 //VID
-// #define USB_CFG_DEVICE_ID 0x50, 0x02 //PID
-// #define USB_CFG_VENDOR_NAME     'A','p','p','l','e'
-// #define USB_CFG_VENDOR_NAME_LEN 5
+#define LED_PIN 1
 
-
-unsigned int LowerCycleTime = 1000; //Minimum Time in milli-seconds between each mouse action  Default: 10000 (10 Seconds), Max 65535ms
-unsigned int UpperCycleTime = 5000; //Maximum Time in milli-seconds between each mouse action  Default: 30000 (30 Seconds), Max 65535ms
+unsigned int LowerCycleTime = 24000; //Minimum Time in milli-seconds between each mouse action  Default: 10000 (10 Seconds), Max 65535ms
+unsigned int UpperCycleTime = 54000; //Maximum Time in milli-seconds between each mouse action  Default: 30000 (30 Seconds), Max 65535ms
 //Random Function will randomly execute a mouse move between these two values
 void setup() {
   randomSeed(analogRead(0));  //Random Seed off background noise on analog pin
-  pinMode(1, OUTPUT); // LED (Note on some older digispark devices, the LED is on pin 0)
-  DigiMouse.begin(); //start
+  pinMode(LED_PIN, OUTPUT); // LED (Note on some older digispark devices, the LED is on pin 0)
+  DigiMouse.begin();
 }
 void loop() {
   digitalWrite(1, HIGH);
   DigiMouse.moveX(2); // Move 2 pixel right
   DigiMouse.moveX(-2); // Move 2 pixel left
   DigiMouse.delay(50);
-  digitalWrite(1, LOW);
+  digitalWrite(LED_PIN, LOW);
   DigiMouse.delay(random(LowerCycleTime, UpperCycleTime));
 }
